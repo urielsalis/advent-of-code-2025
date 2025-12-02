98538bc7e528b9e358677f634b9ebee50d903b5d
urielsalis <uriel@urielsalis.com>
2025-12-02 13:10:51 +0100

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,62 @@
+fun extractMovement(movement: String): Pair<Char, Int> {
+    val direction = movement[0]
+    val count = movement.substring(1).toInt()
+    return Pair(direction, count)
+}
+
+fun adjust(value: Int): Int {
+    return ((value % 100) + 100) % 100
+}
+
+fun move(direction: Char, position: Int, count: Int): Int {
+    return when (direction) {
+        'R' -> adjust(position + count)
+        'L' -> adjust(position - count)
+        else -> error("Invalid direction")
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var position = 50
+        var seenZeros = 0
+        for (movement in input) {
+            val (direction, count) = extractMovement(movement)
+            position = move(direction, position, count)
+            if (position == 0) {
+                seenZeros++
+            }
+        }
+        return seenZeros
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var position = 50
+        var passThroughZeros = 0
+        for (movement in input) {
+            val (direction, count) = extractMovement(movement)
+            val stepsToZero = when {
+                position == 0 -> 100
+                direction == 'L' -> position
+                else -> 100 - position
+            }
+            if (count >= stepsToZero) {
+                passThroughZeros++
+                passThroughZeros += (count - stepsToZero) / 100
+            }
+            position = move(direction, position, count)
+        }
+        return passThroughZeros
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Part 1: " + part1(input)) // 1158
+    println("Part 2: " + part2(input)) // 6860
+    check(part1(input) == 1158)
+    check(part2(input) == 6860)
 }
